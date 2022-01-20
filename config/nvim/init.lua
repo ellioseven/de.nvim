72baac4eb96f282d0a82e5e712cd504da45344cd
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2022-01-20 20:25:18 +1100

    Move to lua

---
+++ config/nvim/init.lua
@@ -0,0 +1,9 @@
+--
+-- init.lua
+--
+-- install with:
+-- $ nvim -u ~/.config/nvim/lua/plugins.lua --headless \
+--   "-c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+require("plugins")
+require("profile")
--- config/nvim/init.vim
+++
@@ -1,8 +0,0 @@
-"
-" .vimrc
-"
-" install with:
-" $ nvim -u ~/.config/nvim/lua/plugins.lua --headless \
-"   "-c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-lua require("plugins")
-lua require("profile")
