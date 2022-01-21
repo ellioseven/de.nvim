9bc464f058299d502cadc504ba7150213f9be99f
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2022-01-21 19:58:08 +1100

    Add search in buffer

--- config/nvim/lua/profile.lua
+++ config/nvim/lua/profile.lua
@@ -215,6 +215,7 @@ map('n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>')
 map('n', '<leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<cr>')
 map('n', '<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>')
 map('n', '<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>')
+map('n', '<leader>fs', '<cmd>lua require("telescope.builtin").current_buffer_fuzzy_find()<cr>')
 
 -- plugin: telescope-coc.nvim
 
