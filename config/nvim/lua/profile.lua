8c63d272a155ec0cee0b90ae7963f257a114d4c7
Elliot Mitchum <elliot.mitchum@ellioseven.com.au>
2022-01-27 07:25:09 +1100

    Add git signs keymap

--- config/nvim/lua/profile.lua
+++ config/nvim/lua/profile.lua
@@ -192,6 +192,16 @@ require('gitsigns').setup({
   current_line_blame = true
 })
 
+map('n', '<leader>hf', '<cmd>Gitsigns next_hunk<CR>')
+map('n', '<leader>hb', '<cmd>Gitsigns prev_hunk<CR>')
+map('n', '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>')
+map('n', '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>')
+map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+map('n', '<leader>hd', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+
 -- plugin: coc.nvim
 
 vim.api.nvim_set_keymap('n', '<leader>dn', '<Plug>(coc-diagnostic-next)', { silent = true })
